017bf22df94ebc901cf74c90f1a1cc3a80680b58
Diego San Román <100495878@alumnos.uc3m.es>
2024-10-25 15:02:02 +0200

    El maxlevel ya va en camino

--- imgaos/maxlevel.cpp
+++ imgaos/maxlevel.cpp
@@ -4,9 +4,10 @@
 #include "maxlevel.hpp"
 
 void performMaxLevelOperation(const std::string& inputFile, const std::string& outputFile, int newMaxValue) {
-  // Aquí va la lógica para realizar la operación de maxlevel
   std::cout << "Realizando la operación de maxlevel en imgsoa con el nuevo valor máximo: " << newMaxValue << "\n";
   std::cout << "Archivo de entrada: " << inputFile << "\n";
   std::cout << "Archivo de salida: " << outputFile << "\n";
-  // Agrega aquí la lógica adicional para la operación de maxlevel.
+
+  // Obtener la intensidad máxima del archivo de entrada y mostrarla en la consola (simulación)
+  
 }
